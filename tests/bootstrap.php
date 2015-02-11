b5675e9b23aebf6893f537164bfbdf5263a6e832
David Guillot <david.guillot@mylittleparis.com>
2015-02-11 13:58:31 +0100

    Made it really PSR-4 compliant this time :)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,5 +5,3 @@ if (!($loader = @include __DIR__.'/../vendor/autoload.php')) {
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install --dev'.PHP_EOL);
 }
-
-$loader->add('MyLittle\\CampaignCommander\\Tests', 'tests');
